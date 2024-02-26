041e94119d85391b4dc651a3ce06a990f98f991e
Sophia Wegener <sumpel1@web.de>
2024-02-26 23:08:18 +0100

    Taschenrechnerprojekt upload

---
+++ Taschenrechner/InfixToPrefixConverter.cs
@@ -0,0 +1,66 @@
+
+public class InfixToPrefixConverter
+{
+
+    private static readonly Dictionary<char, int> Precedence = new Dictionary<char, int>
+    {
+        {'+', 1}, {'-', 1},
+        {'*', 2}, {'/', 2},
+        {'^', 3}
+    };
+
+    //function to convert infix form to prefix form
+    public static string[] InfixToPrefix(string[] infix)
+    {
+        Stack<string> operators = new Stack<string>();
+        Stack<string> operands = new Stack<string>();
+
+        Array.Reverse(infix);
+
+        foreach (string token in infix)
+        {
+            if (IsOperand(token))
+            {
+                operands.Push(token);
+            }
+            else if (token == ")")
+            {
+                operators.Push(token);
+            }
+            else if (token == "(")
+            {
+                while (operators.Peek() != ")")
+                {
+                    operands.Push(operators.Pop());
+                }
+                operators.Pop(); // Pop the ")"
+            }
+            else if (IsOperator(token))
+            {
+                while (operators.Count > 0 && operators.Peek() != ")" && Precedence[operators.Peek()[0]] > Precedence[token[0]])
+                {
+                    operands.Push(operators.Pop());
+                }
+                operators.Push(token);
+            }
+        }
+
+        while (operators.Count > 0)
+        {
+            operands.Push(operators.Pop());
+        }
+
+        return operands.ToArray();
+    }
+
+    private static bool IsOperand(string token)
+    {
+        double result;
+        return double.TryParse(token, out result);
+    }
+
+    public static bool IsOperator(string token)
+    {
+        return token.Length == 1 && (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/' || token[0] == '^');
+    }
+}
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Taschenrechner;
+
+// TODO: unbekannter Operator abfangen (check function mit allem was vorher abgefangen wird), double Problem lösen oder double abfangen, ReadMe
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Geben Sie eine Rechnung ein (z.B.: 4 + 5.3 * -2), oder 'exit' zum beenden");
+            string input = Console.ReadLine(); // reads term
+
+            // check for empty or only whitespaces
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Ihre Eingabe ist ungültig.");
+                continue;
+            }
+
+            // exit case
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+
+            // term into an array
+            string[] termSplit = input.Split(' ');
+
+            // TODO: oneline
+            double result = Calculate(InfixToPrefixConverter.InfixToPrefix(termSplit));
+            Console.WriteLine($"Ergebnis: {result}");
+        }
+    }
+
+    // function to calculate the term
+    public static double Calculate(string[] prefix)
+    {
+        Stack<double> stack = new Stack<double>();
+
+        for (int i = prefix.Length - 1; i >= 0; i--)
+        {
+            if (InfixToPrefixConverter.IsOperator(prefix[i]))
+            {
+                double operand1 = stack.Pop();
+                double operand2 = stack.Pop();
+
+                switch (prefix[i])
+                {
+                    case "+":
+                        stack.Push(operand1 + operand2);
+                        break;
+                    case "-":
+                        stack.Push(operand1 - operand2);
+                        break;
+                    case "*":
+                        stack.Push(operand1 * operand2);
+                        break;
+                    case "/":
+                        if (operand2 != 0)
+                        {
+                            stack.Push(operand1 / operand2);
+                        }
+                        else
+                        {
+                            throw new DivideByZeroException("Teilung durch Null nicht möglich");
+                        }
+                        break;
+                    case "^":
+                        stack.Push(Math.Pow(operand1, operand2));
+                        break;
+                }
+            }
+            else
+            {
+                stack.Push(double.Parse(prefix[i]));
+            }
+        }
+
+        return stack.Pop();
+    }
+}
